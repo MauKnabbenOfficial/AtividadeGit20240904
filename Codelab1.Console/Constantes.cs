e890357da14404616ea6aa3be57fa795c1cd6632
leopellizzoni <lpellizzoni@ucs.br>
2024-09-02 21:03:34 -0300

    Cria projeto de teste para ser usado no codelab1

---
+++ Codelab1.Console/Constantes.cs
@@ -0,0 +1,7 @@
+﻿namespace Codelab1.Console
+{
+    internal class Constantes
+    {
+        public const String HelloWorldMsg = "Hello World!";
+    }
+}
---
+++ Codelab1.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Codelab1.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(Constantes.HelloWorldMsg);
+        }
+    }
+}
